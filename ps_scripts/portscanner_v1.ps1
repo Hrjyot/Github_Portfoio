858bc3860ea2ab75136c47ecdcd401ba8528e169
Hrjyot <126209679+Hrjyot@users.noreply.github.com>
2024-12-09 14:44:32 -0500

    Add files via upload

---
+++ ps_scripts/portscanner_v1.ps1
@@ -0,0 +1,10 @@
+﻿$ipAddress = "localhost"
+$startPort = 130
+$endPort = 140
+
+for ($port = $startPort; $port -le $endPort; $port++) {
+    $result = Test-NetConnection -ComputerName $ipAddress -Port $port
+    if ($result.TcpTestSucceeded) {
+        Write-Output "Port $port is open on $ipAddress"
+    }
+}
---
+++ ps_scripts/portscanner_v2.ps1
@@ -0,0 +1,22 @@
+﻿$ipAddress = "localhost"
+$startPort = 1
+$endPort = 999
+
+for ($port = $startPort; $port -le $endPort; $port++) {
+    $tcpConnection = New-Object System.Net.Sockets.TcpClient
+    try {
+        # Attempt to connect to the port
+        $tcpConnection.Connect($ipAddress, $port)
+        
+        # If connection is successful, port is open
+        if ($tcpConnection.Connected) {
+            Write-Output "Port $port is open on $ipAddress"
+        }
+    } catch {
+        # If the connection fails, port is closed
+        Write-Output "Port $port is closed on $ipAddress"
+    } finally {
+        # Close the TCP connection
+        $tcpConnection.Close()
+    }
+}
